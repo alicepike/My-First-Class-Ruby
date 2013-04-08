d3149fc7e05ea1405374578647de2fb707b34f1e
Alice Pike <alicepike123@hotmail.com>
2013-04-08 20:51:33 +0100

    homework

---
+++ my_first_class.rb
@@ -0,0 +1,82 @@
+ class Sentence
+ 	def initialize(name,content)
+ 		@name = name
+ 		@content = content
+ 	end
+ 	def name
+ 		@name
+ 	end
+ 	def content
+ 		@content
+ 	end
+
+ end
+
+ class SentencePersistence
+ 	def add(sentence)
+
+ 		File.open(file_name,"w+") do |file|
+ 			file.write(sentence.content())
+ 		end
+ 	end
+ 	def to_s
+ 		"<An instance of sentence persistence>"
+ 	end
+
+ end
+
+ class SentenceUi
+ 	def initialize(sentence_store,io)
+ 		@store = sentence_store
+ 		@io = io
+ 	end
+ 	def main_menu
+ 		@io.puts("Welcome to sentence store v1")
+ 		@io.puts("Do you want to (s) store a sentence (r) retrieve a sentence?:")
+
+ 		answer = gets()
+
+ 		if answer == "s"
+ 			puts("Name of sentence")
+			sentence_name = gets().chomp()
+
+			puts = "Content of sentence"
+			sentence_content = gets().chomp()
+
+			sentence = Sentence.new(sentence_name,sentence_content)
+
+			@store.add(sentence)
+
+			puts("Sentence #{sentence.name()} has been stored!")
+
+ 		elsif answer == "r"
+
+ 			# ask store to retrieve sentence given it's name
+ 			puts("Name of sentence")
+ 			sentence_name = gets().chomp()
+ 			# give nice error message if we don't know about the sentence
+ 			# display sentence in some format to user
+
+ 		elsif answer == "l"
+
+ 			# list all sentences present
+ 			# or tell user if there are none
+
+ 		else 
+ 			@io.puts("Sorry I have no idea how to #{answer} a sentence")
+ 		end
+ 	end
+ end
+
+ def main(io)
+
+ 	sentence_ui = SentenceUi.new(
+ 		SentencePersistence.new(),
+ 		io
+ 		)
+
+ 	sentence_ui.main_menu()
+
+ end
+
+ main(STDOUT)
